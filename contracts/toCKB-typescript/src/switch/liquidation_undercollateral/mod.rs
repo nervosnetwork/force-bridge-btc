5f16adfb5398680ea9fdb4bc015e8bf1493d8b25
Ethan Hu <fendouhyz@163.com>
2020-08-25 16:02:39 +0800

    fix: rebase to fit new data structure

--- contracts/toCKB-typescript/src/switch/liquidation_undercollateral/mod.rs
+++ contracts/toCKB-typescript/src/switch/liquidation_undercollateral/mod.rs
@@ -1,33 +1,44 @@
 use crate::switch::ToCKBCellDataTuple;
-use crate::utils::types::{
-    Error, XChainKind,
+use crate::utils::{
+    types::{Error, ToCKBCellDataView},
+    tools::{XChainKind, get_xchain_kind},
 };
 use core::result::Result;
 use ckb_std::{
     ckb_constants::Source,
-    high_level::{load_cell_capacity, load_input_since},
+    high_level::load_cell_capacity,
 };
 
 pub fn verify(toCKB_data_tuple: &ToCKBCellDataTuple) -> Result<(), Error> {
     let input_data = toCKB_data_tuple.0.as_ref().expect("inputs should contain toCKB cell");
     let output_data = toCKB_data_tuple.1.as_ref().expect("outputs should contain toCKB cell");
 
-    // check if capacity and data[2..5] are not changed
+    verify_capacity()?;
+    verify_data(input_data, output_data)?;
+    check_undercollateral()?;
+
+    Ok(())
+}
+
+fn verify_capacity() -> Result<(), Error> {
     let cap_input = load_cell_capacity(0, Source::GroupInput).expect("get input capacity");
     let cap_output = load_cell_capacity(0, Source::GroupOutput).expect("get output capacity");
     if cap_input != cap_output {
         return Err(Error::CapacityInvalid);
     }
+    Ok(())
+}
 
-    if input_data.kind != output_data.kind
-        || input_data.user_lockscript_hash.as_ref() != output_data.user_lockscript_hash.as_ref()
+fn verify_data(input_data: &ToCKBCellDataView, output_data: &ToCKBCellDataView) -> Result<(), Error> {
+    if input_data.user_lockscript.as_ref() != output_data.user_lockscript.as_ref()
         || input_data.x_lock_address.as_ref() != output_data.x_lock_address.as_ref()
-        || input_data.signer_lockscript_hash.as_ref() != output_data.signer_lockscript_hash.as_ref()
+        || input_data.signer_lockscript.as_ref() != output_data.signer_lockscript.as_ref()
     {
         return Err(Error::InvariantDataMutated);
     }
 
-    match input_data.kind {
+    let xchain_kind = get_xchain_kind()?;
+    match xchain_kind {
         XChainKind::Btc => {
             if input_data.get_btc_lot_size()? != output_data.get_btc_lot_size()? {
                 return Err(Error::InvariantDataMutated);
@@ -39,8 +50,6 @@ pub fn verify(toCKB_data_tuple: &ToCKBCellDataTuple) -> Result<(), Error> {
             }
         }
     };
-
-    check_undercollateral()?;
 
     Ok(())
 }
