f5cf9d86d626a8bfa9e2f3fe0e675a7eb85d6f0a
Ethan Hu <fendouhyz@163.com>
2020-08-28 16:42:49 +0800

    fix: modify SINCE_SIGNER_TIMEOUT to 1 day that equal 24 * 3600, the unit being second

--- tests/src/toCKB_typescript/utils/config.rs
+++ tests/src/toCKB_typescript/utils/config.rs
@@ -4,5 +4,5 @@ pub const PLEDGE: u64 = 10000;
 pub const LOCK_TYPE_FLAG: u64 = 1 << 63;
 pub const SINCE_TYPE_TIMESTAMP: u64 = 0x4000_0000_0000_0000;
 
-// 3 * 24 * 3600 means 3 days, the unit is millisecond
-pub const SINCE_SIGNER_TIMEOUT: u64 = LOCK_TYPE_FLAG | SINCE_TYPE_TIMESTAMP | 3 * 24 * 3600;
+// 24 * 3600 means 1 day, the unit is second
+pub const SINCE_SIGNER_TIMEOUT: u64 = LOCK_TYPE_FLAG | SINCE_TYPE_TIMESTAMP | 24 * 3600;
