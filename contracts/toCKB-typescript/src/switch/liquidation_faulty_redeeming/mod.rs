3d4f76acda2caf8df904fa66146f27e6a5d018d2
Ethan Hu <fendouhyz@163.com>
2020-08-28 22:36:37 +0800

    fix: rebase to fit new code

--- contracts/toCKB-typescript/src/switch/liquidation_faulty_redeeming/mod.rs
+++ contracts/toCKB-typescript/src/switch/liquidation_faulty_redeeming/mod.rs
@@ -1,16 +1,17 @@
 use crate::switch::ToCKBCellDataTuple;
-use crate::utils::types::{
-    Error, XChainKind, ToCKBCellDataView
-};
+use crate::utils::types::{Error, ToCKBCellDataView};
+use ckb_std::{ckb_constants::Source, high_level::load_cell_capacity};
 use core::result::Result;
-use ckb_std::{
-    ckb_constants::Source,
-    high_level::{load_cell_capacity},
-};
 
 pub fn verify(toCKB_data_tuple: &ToCKBCellDataTuple) -> Result<(), Error> {
-    let input_data = toCKB_data_tuple.0.as_ref().expect("inputs should contain toCKB cell");
-    let output_data = toCKB_data_tuple.1.as_ref().expect("outputs should contain toCKB cell");
+    let input_data = toCKB_data_tuple
+        .0
+        .as_ref()
+        .expect("inputs should contain toCKB cell");
+    let output_data = toCKB_data_tuple
+        .1
+        .as_ref()
+        .expect("outputs should contain toCKB cell");
 
     verify_capacity()?;
     verify_data(input_data, output_data)?;
@@ -32,35 +33,19 @@ fn verify_data(
     input_data: &ToCKBCellDataView,
     output_data: &ToCKBCellDataView,
 ) -> Result<(), Error> {
-    // check if data[2..7] are not changed
-    if input_data.kind != output_data.kind
-        || input_data.user_lockscript_hash.as_ref() != output_data.user_lockscript_hash.as_ref()
+    if input_data.get_raw_lot_size() != output_data.get_raw_lot_size()
+        || input_data.user_lockscript.as_ref() != output_data.user_lockscript.as_ref()
         || input_data.x_lock_address.as_ref() != output_data.x_lock_address.as_ref()
-        || input_data.signer_lockscript_hash.as_ref() != output_data.signer_lockscript_hash.as_ref()
+        || input_data.signer_lockscript.as_ref() != output_data.signer_lockscript.as_ref()
         || input_data.x_unlock_address.as_ref() != output_data.x_unlock_address.as_ref()
-        || input_data.redeemer_lockscript_hash.as_ref() != output_data.redeemer_lockscript_hash.as_ref()
+        || input_data.redeemer_lockscript.as_ref() != output_data.redeemer_lockscript.as_ref()
     {
         return Err(Error::InvariantDataMutated);
     }
 
-    match input_data.kind {
-        XChainKind::Btc => {
-            if input_data.get_btc_lot_size()? != output_data.get_btc_lot_size()? {
-                return Err(Error::InvariantDataMutated);
-            }
-        }
-        XChainKind::Eth => {
-            if input_data.get_eth_lot_size()? != output_data.get_eth_lot_size()? {
-                return Err(Error::InvariantDataMutated);
-            }
-        }
-    };
-
     Ok(())
 }
 
-
 fn verify_witness() -> Result<(), Error> {
-
     Ok(())
 }
