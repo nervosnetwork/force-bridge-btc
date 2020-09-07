581437df5a508219d4c51de37a5f50cb213c487f
Ethan Hu <fendouhyz@163.com>
2020-09-07 14:06:19 +0800

    feat: add capacity units and add sudt_amount check in Auction: FaultyWhenWarranty

--- contracts/toCKB-typescript/src/switch/auction_faulty_warranty/mod.rs
+++ contracts/toCKB-typescript/src/switch/auction_faulty_warranty/mod.rs
@@ -4,7 +4,7 @@ use crate::utils::{
         AUCTION_INIT_PERCENT, AUCTION_MAX_TIME, LOCK_TYPE_FLAG, METRIC_TYPE_FLAG_MASK,
         REMAIN_FLAGS_BITS, SINCE_TYPE_TIMESTAMP, UDT_LEN, VALUE_MASK, XT_CELL_CAPACITY,
     },
-    tools::{get_xchain_kind, is_XT_typescript, XChainKind},
+    tools::{get_sum_sudt_amount, get_xchain_kind, is_XT_typescript, XChainKind},
     types::{Error, ToCKBCellDataView},
 };
 use ckb_std::{
@@ -40,9 +40,15 @@ pub fn verify(toCKB_data_tuple: &ToCKBCellDataTuple) -> Result<(), Error> {
     debug!("begin verify since");
     let auction_time = verify_since()?;
     debug!("begin verify input");
-    verify_inputs(toCKB_lock_hash.as_ref(), lot_amount)?;
+    let inputs_xt_amount = verify_inputs(toCKB_lock_hash.as_ref(), lot_amount)?;
     debug!("begin verify output");
-    verify_outputs(input_data, auction_time)?;
+    verify_outputs(
+        input_data,
+        inputs_xt_amount,
+        auction_time,
+        toCKB_lock_hash.as_ref(),
+        lot_amount,
+    )?;
 
     Ok(())
 }
@@ -62,36 +68,25 @@ fn verify_since() -> Result<u64, Error> {
     Ok(auction_time)
 }
 
-fn verify_inputs(toCKB_lock_hash: &[u8], lot_amount: u128) -> Result<(), Error> {
+fn verify_inputs(toCKB_lock_hash: &[u8], lot_amount: u128) -> Result<u128, Error> {
+    // inputs[0]: toCKB cell
+    // inputs[1:]: XT cell the bidder provides
     // check XT cell on inputs
-    let mut input_index = 1;
-    let mut sum_amount = 0;
-    loop {
-        let res = load_cell_type(input_index, Source::Input);
-        if res.is_err() {
-            break;
-        }
-        let script = res.unwrap();
-        if script.is_none() || !is_XT_typescript(&script.unwrap(), toCKB_lock_hash) {
-            return Err(Error::InvalidInputs);
-        }
+    let inputs_amount = get_sum_sudt_amount(1, Source::Input, toCKB_lock_hash)?;
 
-        let cell_data = load_cell_data(input_index, Source::Input)?;
-        let mut data = [0u8; UDT_LEN];
-        data.copy_from_slice(&cell_data);
-        let amount = u128::from_le_bytes(data);
-        sum_amount += amount;
-
-        input_index += 1;
-    }
-
-    if sum_amount < lot_amount {
+    if inputs_amount < lot_amount {
         return Err(Error::FundingNotEnough);
     }
-    Ok(())
+    Ok(inputs_amount)
 }
 
-fn verify_outputs(input_data: &ToCKBCellDataView, auction_time: u64) -> Result<(), Error> {
+fn verify_outputs(
+    input_data: &ToCKBCellDataView,
+    inputs_xt_amount: u128,
+    auction_time: u64,
+    toCKB_lock_hash: &[u8],
+    lot_amount: u128,
+) -> Result<(), Error> {
     // check bidder cell
     debug!("begin check bidder cell");
     debug!(
@@ -109,16 +104,14 @@ fn verify_outputs(input_data: &ToCKBCellDataView, auction_time: u64) -> Result<(
 
     // expect paying ckb to bidder,trigger and signer
     // cap of toCKB_cell ==  XT_CELL_CAPACITY + to_bidder + to_trigger + to_signer
-    let collateral = load_cell_capacity(0, Source::GroupInput)? - XT_CELL_CAPACITY;
-    let mut to_bidder = collateral;
-    let init_collateral = collateral * AUCTION_INIT_PERCENT as u64 / 100;
-    if auction_time == 0 {
-        to_bidder = init_collateral
-    } else if auction_time < AUCTION_MAX_TIME {
+    let asset_collateral = load_cell_capacity(0, Source::GroupInput)? - XT_CELL_CAPACITY;
+    let mut to_bidder = asset_collateral;
+    let init_collateral = asset_collateral * AUCTION_INIT_PERCENT as u64 / 100;
+    if auction_time < AUCTION_MAX_TIME {
         to_bidder =
-            init_collateral + (collateral - init_collateral) * auction_time / AUCTION_MAX_TIME
+            init_collateral + (asset_collateral - init_collateral) / AUCTION_MAX_TIME * auction_time
     }
-    let to_trigger = collateral - to_bidder + XT_CELL_CAPACITY;
+    let to_trigger = asset_collateral - to_bidder + XT_CELL_CAPACITY;
 
     // - 2. check the repayment to bidder
     // expect bidder_cell_cap == repayment_to_bidder + (cap_sum of inputs_xt_cell)
@@ -127,7 +120,7 @@ fn verify_outputs(input_data: &ToCKBCellDataView, auction_time: u64) -> Result<(
     }
     debug!("2. check bidder repayment success! ");
 
-    debug!("collateral: {}, ", collateral);
+    debug!("collateral: {}, ", asset_collateral);
     debug!("to_bidder: {}, to_trigger: {}", to_bidder, to_trigger);
 
     // check trigger cell
@@ -140,12 +133,11 @@ fn verify_outputs(input_data: &ToCKBCellDataView, auction_time: u64) -> Result<(
         return Err(Error::InvalidTriggerOrSignerCell);
     }
 
-    // check no other output cell
-    output_index += 1;
-    if load_cell_capacity(output_index, Source::Output).is_ok() {
-        return Err(Error::InvalidOutputsNum);
+    // - check XT change, make sure inputs_sudt_amount == outputs_sudt_amount
+    let outputs_xt_amount = get_sum_sudt_amount(output_index + 1, Source::Output, toCKB_lock_hash)?;
+    if inputs_xt_amount - outputs_xt_amount != lot_amount {
+        return Err(Error::XTAmountInvalid);
     }
-    debug!("4. check no other output cell success!");
 
     Ok(())
 }
--- tests/src/toCKB_typescript/tests/auction_faulty_warranty.rs
+++ tests/src/toCKB_typescript/tests/auction_faulty_warranty.rs
@@ -20,7 +20,7 @@ const MAX_CYCLES: u64 = 10_000_000;
 fn test_correct_tx_max_time() {
     let since_max_auction_time = LOCK_TYPE_FLAG | SINCE_TYPE_TIMESTAMP | AUCTION_MAX_TIME;
     let (context, tx) =
-        build_test_context(375_0000, 375_0000, 0, since_max_auction_time, 2500_0000);
+        build_test_context(3_750_000, 3_750_000, 0, since_max_auction_time, 25_000_000);
 
     let cycles = context
         .verify_tx(&tx, MAX_CYCLES)
@@ -32,15 +32,15 @@ fn test_correct_tx_max_time() {
 fn test_correct_tx_trigger() {
     let auction_time = 2 * 24 * 3600;
     let since = LOCK_TYPE_FLAG | SINCE_TYPE_TIMESTAMP | auction_time;
-    let collateral = 375_0000u64;
+    let collateral = 3_750_000u64;
     let bidder_cap = {
         let init_repayment = collateral * AUCTION_INIT_PERCENT as u64 / 100;
-        init_repayment + (collateral - init_repayment) * auction_time / AUCTION_MAX_TIME
+        init_repayment + (collateral - init_repayment) / AUCTION_MAX_TIME * auction_time
     };
 
     let trigger_cap = collateral - bidder_cap;
 
-    let (context, tx) = build_test_context(collateral, bidder_cap, trigger_cap, since, 2500_0000);
+    let (context, tx) = build_test_context(collateral, bidder_cap, trigger_cap, since, 25_000_000);
 
     let cycles = context
         .verify_tx(&tx, MAX_CYCLES)
@@ -51,7 +51,7 @@ fn test_correct_tx_trigger() {
 #[test]
 fn test_wrong_since() {
     let since = LOCK_TYPE_FLAG | AUCTION_MAX_TIME;
-    let (context, tx) = build_test_context(375_0000, 375_0000, 0, since, 2500_0000);
+    let (context, tx) = build_test_context(3_750_000, 3_750_000, 0, since, 25_000_000);
 
     let err = context.verify_tx(&tx, MAX_CYCLES).unwrap_err();
     assert_error_eq!(
@@ -64,7 +64,7 @@ fn test_wrong_since() {
 fn test_wrong_XT() {
     let since = LOCK_TYPE_FLAG | SINCE_TYPE_TIMESTAMP | AUCTION_MAX_TIME;
     let wrong_lot_amount: u128 = 999;
-    let (context, tx) = build_test_context(375_0000, 375_0000, 0, since, wrong_lot_amount);
+    let (context, tx) = build_test_context(3_750_000, 3_750_000, 0, since, wrong_lot_amount);
 
     let err = context.verify_tx(&tx, MAX_CYCLES).unwrap_err();
     assert_error_eq!(
@@ -76,9 +76,9 @@ fn test_wrong_XT() {
 #[test]
 fn test_wrong_bidder_cell() {
     let since = LOCK_TYPE_FLAG | SINCE_TYPE_TIMESTAMP | (2 * 24 * 3600);
-    let collateral = 375_0000;
+    let collateral = 3_750_000;
     let wrong_bidder_capacity = 0;
-    let (context, tx) = build_test_context(collateral, wrong_bidder_capacity, 0, since, 2500_0000);
+    let (context, tx) = build_test_context(collateral, wrong_bidder_capacity, 0, since, 25_000_000);
 
     let err = context.verify_tx(&tx, MAX_CYCLES).unwrap_err();
     assert_error_eq!(
@@ -91,14 +91,15 @@ fn test_wrong_bidder_cell() {
 fn test_wrong_trigger() {
     let time = 2 * 24 * 3600;
     let since = LOCK_TYPE_FLAG | SINCE_TYPE_TIMESTAMP | time;
-    let collateral = 375_0000u64;
+    let collateral = 3_750_000u64;
     let bidder_cap = {
         let init_repayment = collateral * AUCTION_INIT_PERCENT as u64 / 100;
         init_repayment + (collateral - init_repayment) * time / AUCTION_MAX_TIME
     };
 
     let wrong_trigger = 10;
-    let (context, tx) = build_test_context(collateral, bidder_cap, wrong_trigger, since, 2500_0000);
+    let (context, tx) =
+        build_test_context(collateral, bidder_cap, wrong_trigger, since, 25_000_000);
 
     let err = context.verify_tx(&tx, MAX_CYCLES).unwrap_err();
     assert_error_eq!(
