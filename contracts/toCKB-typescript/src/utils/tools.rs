6980bebcdf3910a4ac14cb0cedebed188f0488ab
jacobdenver007 <jacobdenver@163.com>
2020-09-07 11:20:27 +0800

    fix(tools): add get_prce

--- contracts/toCKB-typescript/src/utils/tools.rs
+++ contracts/toCKB-typescript/src/utils/tools.rs
@@ -1,6 +1,7 @@
 use super::types::Error;
+use ckb_std::ckb_constants::Source;
 use ckb_std::ckb_types::{bytes::Bytes, prelude::Unpack};
-use ckb_std::high_level::load_script;
+use ckb_std::high_level::{load_cell_data, load_script};
 use core::result::Result;
 use int_enum::IntEnum;
 
@@ -20,4 +21,20 @@ pub fn get_xchain_kind() -> Result<XChainKind, Error> {
     buf.copy_from_slice(script_args.as_ref());
     let kind = u8::from_le_bytes(buf);
     XChainKind::from_int(kind).map_err(|_| Error::Encoding)
+}
+
+pub fn get_price(kind: XChainKind) -> Result<u128, Error> {
+    let price_cell_data = load_cell_data(0, Source::CellDep)?;
+    if price_cell_data.len() != 16 {
+        return Err(Error::Encoding);
+    }
+    let mut buf = [0u8; 16];
+    buf.copy_from_slice(&price_cell_data);
+    let price: u128 = u128::from_le_bytes(buf);
+
+    match kind {
+        XChainKind::Btc => todo!(),
+        XChainKind::Eth => todo!(),
+    }
+    Ok(price)
 }
