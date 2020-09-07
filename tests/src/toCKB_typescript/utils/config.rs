a205ac7686b189451301de4da1dc3c41b506b902
Ethan Hu <fendouhyz@163.com>
2020-09-07 11:04:08 +0800

    feat: add func to tools, extract verify_btc_witness to tools

--- tests/src/toCKB_typescript/utils/config.rs
+++ tests/src/toCKB_typescript/utils/config.rs
@@ -1,3 +1,4 @@
+pub const UDT_LEN: usize = 16;
 pub const CKB_UNITS: u64 = 100_000_000;
 pub const PLEDGE: u64 = 10000 * CKB_UNITS;
 pub const XT_CELL_CAPACITY: u64 = 200 * CKB_UNITS;
@@ -13,9 +14,20 @@ pub const SUDT_CODE_HASH: [u8; 32] = [
 // since
 pub const LOCK_TYPE_FLAG: u64 = 1 << 63;
 pub const SINCE_TYPE_TIMESTAMP: u64 = 0x4000_0000_0000_0000;
+pub const METRIC_TYPE_FLAG_MASK: u64 = 0x6000_0000_0000_0000;
+pub const VALUE_MASK: u64 = 0x00ff_ffff_ffff_ffff;
+pub const REMAIN_FLAGS_BITS: u64 = 0x1f00_0000_0000_0000;
 
 // 24 * 3600 means 1 day, the unit is second
 pub const SINCE_SIGNER_TIMEOUT: u64 = LOCK_TYPE_FLAG | SINCE_TYPE_TIMESTAMP | 24 * 3600;
 
 // LIQUIDATION_COLLATERAL_PERCENT means min liquidation threshold of collateral/lot_amount
 pub const LIQUIDATION_COLLATERAL_PERCENT: u8 = 115;
+
+/*
+If auction tx executed after AUCTION_MAX_TIME, the bidder need to pay the
+lowest price that equals 'lot_amount/collateral' to get the collateral.
+The time unit is second.
+*/
+pub const AUCTION_MAX_TIME: u64 = 3 * 24 * 3600;
+pub const AUCTION_INIT_PERCENT: u8 = 67;
