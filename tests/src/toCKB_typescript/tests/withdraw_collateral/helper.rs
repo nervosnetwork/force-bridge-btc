8484207f2547c4eff9b7138ca09c97c14d6a9659
jacobdenver007 <jacobdenver@163.com>
2020-09-07 15:05:43 +0800

    fix(withdraw collateral): add tests

---
+++ tests/src/toCKB_typescript/tests/withdraw_collateral/helper.rs
@@ -0,0 +1,149 @@
+use super::{types::*, ToCKBCellData};
+use crate::toCKB_typescript::utils::types::{
+    generated::{basic, btc_difficulty, mint_xt_witness},
+    ToCKBStatus,
+};
+use crate::*;
+use ckb_testtool::{builtin::ALWAYS_SUCCESS, context::Context};
+use ckb_tool::ckb_types::{bytes::Bytes, core::TransactionBuilder, packed::*, prelude::*};
+use molecule::prelude::*;
+
+pub const MAX_CYCLES: u64 = 100_000_000;
+
+pub fn run_test_case(case: TestCase) {
+    let kind = case.kind;
+
+    let mut context = Context::default();
+    let toCKB_typescript_bin: Bytes = Loader::default().load_binary("toCKB-typescript");
+    let toCKB_typescript_out_point = context.deploy_cell(toCKB_typescript_bin);
+    let always_success_out_point = context.deploy_cell(ALWAYS_SUCCESS.clone());
+
+    // prepare scripts
+    let toCKB_typescript = context
+        .build_script(&toCKB_typescript_out_point, [kind; 1].to_vec().into())
+        .expect("script");
+    let toCKB_typescript_dep = CellDep::new_builder()
+        .out_point(toCKB_typescript_out_point)
+        .build();
+    let always_success_lockscript = context
+        .build_script(&always_success_out_point, Default::default())
+        .expect("script");
+    let always_success_lockscript_dep = CellDep::new_builder()
+        .out_point(always_success_out_point)
+        .build();
+
+    // prepare cells
+    let x_unlock_address_str = case.tockb_cell_data.x_unlock_address;
+    let x_unlock_address = basic::Bytes::new_builder()
+        .set(
+            x_unlock_address_str
+                .as_bytes()
+                .iter()
+                .map(|c| Byte::new(*c))
+                .collect::<Vec<_>>()
+                .into(),
+        )
+        .build();
+    let signer_lockscript =
+        basic::Script::from_slice(case.tockb_cell_data.signer_lockscript.as_slice()).unwrap();
+    let user_lockscript =
+        basic::Script::from_slice(case.tockb_cell_data.user_lockscript.as_slice()).unwrap();
+    let input_toCKB_data = ToCKBCellData::new_builder()
+        .status(Byte::new(ToCKBStatus::Redeeming as u8))
+        .lot_size(Byte::new(case.tockb_cell_data.lot_size))
+        .signer_lockscript(signer_lockscript.clone())
+        .user_lockscript(user_lockscript.clone())
+        .x_unlock_address(x_unlock_address.clone())
+        .build();
+
+    let input_ckb_cell_out_point = context.create_cell(
+        CellOutput::new_builder()
+            .capacity(case.input_capacity.pack())
+            .lock(always_success_lockscript.clone())
+            .type_(Some(toCKB_typescript.clone()).pack())
+            .build(),
+        input_toCKB_data.as_bytes(),
+    );
+    let input_ckb_cell = CellInput::new_builder()
+        .previous_output(input_ckb_cell_out_point)
+        .build();
+    let inputs = vec![input_ckb_cell];
+    let outputs = vec![CellOutput::new_builder()
+        .capacity(case.output_capacity.pack())
+        .lock(always_success_lockscript.clone())
+        .build()];
+    let outputs_data = vec![Bytes::new(); 1];
+
+    let spv_proof = match case.witness.spv_proof {
+        SpvProof::BTC(btc_spv_proof) => btc_spv_proof.as_slice().to_vec(),
+    };
+    let witness_data = mint_xt_witness::MintXTWitness::new_builder()
+        .spv_proof(spv_proof.into())
+        .cell_dep_index_list(case.witness.cell_dep_index_list.into())
+        .build();
+    let witness = WitnessArgs::new_builder()
+        .input_type(Some(witness_data.as_bytes()).pack())
+        .build();
+    let dep_data = match case.cell_deps_data {
+        CellDepsData::BTC(difficulty_data) => {
+            let data = btc_difficulty::BTCDifficulty::new_builder()
+                .previous(difficulty_data.previous.to_le_bytes().to_vec().into())
+                .current(difficulty_data.current.to_le_bytes().to_vec().into())
+                .build();
+            dbg!(&data);
+            data.as_bytes()
+        }
+    };
+    let data_out_point = context.deploy_cell(dep_data);
+    let data_dep = CellDep::new_builder().out_point(data_out_point).build();
+
+    let tx = TransactionBuilder::default()
+        .inputs(inputs)
+        .outputs(outputs)
+        .outputs_data(outputs_data.pack())
+        .cell_dep(data_dep)
+        .cell_dep(toCKB_typescript_dep)
+        .cell_dep(always_success_lockscript_dep)
+        .witness(witness.as_bytes().pack())
+        .build();
+    let res = context.verify_tx(&tx, MAX_CYCLES);
+    dbg!(&res);
+    match res {
+        Ok(_cycles) => assert_eq!(case.expect_return_code, 0),
+        Err(err) => assert!(check_err(err, case.expect_return_code)),
+    }
+}
+
+pub fn check_err(err: ckb_tool::ckb_error::Error, code: i8) -> bool {
+    let get = format!("{}", err);
+    let expected = format!("Script(ValidationFailure({}))", code);
+    dbg!(&get, &expected);
+    get == expected
+}
+
+pub struct DeployResult {
+    pub context: Context,
+    pub toCKB_typescript: Script,
+    pub always_success_lockscript: Script,
+}
+
+pub fn deploy(kind: u8) -> DeployResult {
+    let mut context = Context::default();
+    let toCKB_typescript_bin: Bytes = Loader::default().load_binary("toCKB-typescript");
+    let toCKB_typescript_out_point = context.deploy_cell(toCKB_typescript_bin);
+    let always_success_out_point = context.deploy_cell(ALWAYS_SUCCESS.clone());
+
+    // prepare scripts
+    let toCKB_typescript = context
+        .build_script(&toCKB_typescript_out_point, [kind; 1].to_vec().into())
+        .expect("script");
+    let always_success_lockscript = context
+        .build_script(&always_success_out_point, Default::default())
+        .expect("script");
+
+    DeployResult {
+        context,
+        toCKB_typescript,
+        always_success_lockscript,
+    }
+}
---
+++ tests/src/toCKB_typescript/tests/withdraw_collateral/mod.rs
@@ -0,0 +1,102 @@
+mod helper;
+mod types;
+use helper::{deploy, run_test_case, DeployResult};
+use types::*;
+
+use super::ToCKBCellData;
+use crate::toCKB_typescript::utils::types::{generated::mint_xt_witness, Error::*};
+use molecule::prelude::*;
+use std::convert::TryInto;
+
+fn generate_btc_corrent_case() -> TestCase {
+    let kind = 1;
+    let DeployResult {
+        context: _,
+        toCKB_typescript: _,
+        always_success_lockscript,
+    } = deploy(kind);
+    let user_lockscript = always_success_lockscript.clone();
+    let signer_lockscript = always_success_lockscript.clone();
+    let case = TestCase {
+        kind,
+        input_capacity: 100000,
+        output_capacity: 100000,
+        tockb_cell_data: ToCKBCellDataTest {
+            lot_size: 1,
+            x_unlock_address: "bc1qq2pw0kr5yhz3xcs978desw5anfmtwynutwq8quz0t".to_owned(),
+            user_lockscript: user_lockscript.clone(),
+            signer_lockscript: signer_lockscript.clone(),
+        },
+        witness: Witness {
+            cell_dep_index_list: vec![0],
+            spv_proof: SpvProof::BTC(BTCSPVProofJson{
+                version: "0x02000000".to_owned(),
+                vin: "0x015227c5fbad9d9202ade7f02452cf880dac1ed270255ebfe6716e8b3e8956571d0100000017160014085fc2ea0c102fc4db8dbbb10dd6f93684c178c9feffffff".to_owned(),
+                vout: "0x028c79171300000000160014173ec3a12e289b102f8edcc1d4ecd3b5b893e2dc97b2030000000000160014ef9665bcf82fa83e870a350a6551a09ee819e4a3".to_owned(),
+                locktime: "0xdbd80900".to_owned(),
+                tx_id: "0x2b21846ae6f15cc29e41b2846c78d756abfedb0d6fea7222263cac0024713bc3".to_owned(),
+                index: 3,
+                headers: "0x00000020acf05cadf6d066d01f5aca661690f4e1779a8144b90b070000000000000000006bbb5a7851af48d883e8ac5d6f61c6ad9a4132a9a12531c1b6f085760b3b2e427ba0455fea0710177d792e86".to_owned(),
+                intermediate_nodes: "0x8546dfccb488115f9c3210255523c0e186fb9b64d16ac68b3d8903bf037dc3ab26069e90c930cc55105d5f8b4ddd798bc33f057641e748fd2e70de0b8747cae802af46fb1e1fccf354b4b46d87f5a85c564fd5284cbe2a5711c16c446fbb6e9e0b3c7beec06a156a8005883b8cf224f665d361a2269b6b21491c1ccbb8160c311b609b5ca21b0a9f708e6124b36871b71c5536d8d556054be435cf0444da70d0814e678eb0e081805d777f9cf84911f9e04b6a80b6cf60dec31527ec73aaa8ba77ec6bff2e04fbb80c8c81b1cc38b415bc21dd732f51a4a903ee265b0eef2c589f751e66e46bb02aa36ed8418ae93317316b84d12f1b1702dd9641ead0ad7f8777526ad7a4ff599946d219a7a932ec8cd2e42649b3d5fa123d2e4532de6d46bddb27a8c02de8fb8fe2c4d88a14132de8cdd7d471bc6a8c8c217aeec600fd295e8925b663332f45bdb6877dd6e0ecd28bfae530ba3ed8bd3959644a82bc418f9c887746e15ae55d82369c3761187ea449c7f7bdff1acaa0b467e1335b3919089d".to_owned(),
+                funding_output_index: 0,
+            }.try_into().unwrap()),
+        },
+        cell_deps_data: CellDepsData::BTC(
+            BtcDifficultyTest {
+                previous: 17557993035167u64,
+                current: 17557993035167u64,
+            }
+        ),
+        expect_return_code: 0,
+    };
+    case
+}
+
+#[test]
+fn test_btc_correct_case() {
+    let case = generate_btc_corrent_case();
+    run_test_case(case);
+}
+
+#[test]
+fn test_wrong_x_unlock_address() {
+    let mut case = generate_btc_corrent_case();
+    case.tockb_cell_data.x_unlock_address = "wrong_addr".to_owned();
+    case.expect_return_code = WrongFundingAddr as i8;
+    run_test_case(case);
+}
+
+#[test]
+fn test_wrong_cell_dep_index_list_len() {
+    let mut case = generate_btc_corrent_case();
+    case.witness.cell_dep_index_list = vec![1, 2];
+    case.expect_return_code = InvalidWitness as i8;
+    run_test_case(case);
+}
+
+#[test]
+fn test_wrong_btc_spv() {
+    let mut case = generate_btc_corrent_case();
+    case.witness.spv_proof = SpvProof::BTC(mint_xt_witness::BTCSPVProof::default());
+    case.expect_return_code = -1;
+    run_test_case(case);
+}
+
+#[test]
+fn test_wrong_btc_difficulty() {
+    let mut case = generate_btc_corrent_case();
+    case.cell_deps_data = CellDepsData::BTC(BtcDifficultyTest {
+        previous: 1,
+        current: 1,
+    });
+    case.expect_return_code = NotAtCurrentOrPreviousDifficulty as i8;
+    run_test_case(case);
+}
+
+#[test]
+fn test_wrong_toCKB_capacity() {
+    let mut case = generate_btc_corrent_case();
+    case.output_capacity = 9999;
+    case.expect_return_code = CapacityInvalid as i8;
+    run_test_case(case);
+}
