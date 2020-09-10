cc316e668867dc122539cb8e8ef3609fc9a596da
fpChan <fpchen@outlook.com>
2020-09-10 19:29:58 +0800

    add lockscript and test

--- contracts/toCKB-lockscript/src/main.rs
+++ contracts/toCKB-lockscript/src/main.rs
@@ -5,22 +5,20 @@
 #![feature(panic_info_message)]
 #![allow(non_snake_case)]
 
-// Import from `core` instead of from `std` since we are in no-std mode
+mod utils;
+
 use core::result::Result;
-
-// Import heap related library from `alloc`
-// https://doc.rust-lang.org/alloc/index.html
-use alloc::{vec, vec::Vec};
-
-// Import CKB syscalls and structures
-// https://nervosnetwork.github.io/ckb-std/riscv64imac-unknown-none-elf/doc/ckb_std/index.html
 use ckb_std::{
     ckb_types::{bytes::Bytes, prelude::*},
     debug, default_alloc, entry,
     error::SysError,
-    high_level::{load_script, load_tx_hash},
+    high_level::{load_script_hash,load_cell_type_hash,load_cell,load_cell_lock_hash},
+    ckb_constants::Source,
 };
-
+use utils::error::Error;
+use alloc::{vec};
+use hex;
+use ckb_std::high_level::load_script;
 entry!(entry);
 default_alloc!();
 
@@ -33,40 +31,56 @@ fn entry() -> i8 {
     }
 }
 
-/// Error
-#[repr(i8)]
-enum Error {
-    IndexOutOfBound = 1,
-    ItemMissing,
-    LengthNotEnough,
-    Encoding,
-    // Add customized errors here...
+fn main() -> Result<(), Error> {
+    verify()
 }
 
-impl From<SysError> for Error {
-    fn from(err: SysError) -> Self {
-        use SysError::*;
-        match err {
-            IndexOutOfBound => Self::IndexOutOfBound,
-            ItemMissing => Self::ItemMissing,
-            LengthNotEnough(_) => Self::LengthNotEnough,
-            Encoding => Self::Encoding,
-            Unknown(err_code) => panic!("unexpected sys error {}", err_code),
+
+fn verify() -> Result<(), Error> {
+    // load current lock_script hash
+    let script_hash = load_script_hash().unwrap();
+    let args: Bytes  = load_script().unwrap().args().unpack();
+    let cell_source = vec![Source::Input,Source::Output];
+
+    for &source in cell_source.iter() {
+        for i in 0.. {
+            match verify_single_cell(i, source, script_hash, args.clone()) {
+                Ok(()) => {}
+                Err(Error::IndexOutOfBound) => break,
+                Err(err) => return Err(err.into()),
+            };
         }
     }
+    Ok(())
 }
 
-fn main() -> Result<(), Error> {
-    // remove below examples and write your code here
+fn verify_single_cell(index: usize, source: Source, script_hash: [u8; 32], args:Bytes) -> Result<(), Error> {
 
-    let script = load_script()?;
-    let args: Bytes = script.args().unpack();
-    debug!("script args is {:?}", args);
+    let type_hash = match load_cell_type_hash(index, source) {
+        Ok(current_cell_type_hash) => current_cell_type_hash,
+        Err(SysError::IndexOutOfBound) => return Err(Error::IndexOutOfBound),
+        Err(err) => return Err(err.into()),
+    };
 
-    let tx_hash = load_tx_hash()?;
-    debug!("tx hash is {:?}", tx_hash);
+    debug!("test_lock_args : {:?}-{:?} \t lock script args : {:?} \t type script hash : {:?} \n",
+           source, index, hex::encode(&args.to_vec()), hex::encode(type_hash.clone().unwrap()));
 
-    let _buf: Vec<_> = vec![0u8; 32];
+    // the cell is toCKBCell when lock_script args equal typescript hash
+    if args[..] == type_hash.clone().unwrap()[..] {
 
+        let lock_hash = match load_cell_lock_hash(index, source) {
+            Ok(lock_hash) => lock_hash,
+            Err(SysError::IndexOutOfBound) => return Err(Error::IndexOutOfBound),
+            Err(err) => return Err(err.into()),
+        };
+
+        debug!("test_lock_hash : {:?}-{:?} \t lock_script hash : {:?} \t current lock_hash : {:?}\n \n",
+               source, index, hex::encode(lock_hash.clone()), hex::encode(script_hash.clone()));
+
+        //the toCKBCell is valid when the toCKB cell lock_script hash equal current lock_script hash
+        if lock_hash[..] != script_hash[..] {
+            return Err(Error::InvalidToCKBCell);
+        }
+    }
     Ok(())
 }
