c25cdec83282c8461a3d871b0ab29cd54a5a7bc2
huwenchao <me@huwenchao.com>
2020-09-27 03:52:15 +0800

    finish cli

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,9 +1,13 @@
+pub mod commands;
+pub mod server;
+
+use crate::commands::handler;
+use crate::commands::types::Opts;
 use anyhow::{anyhow, Result};
 use clap::Clap;
-use tockb_sdk::commands::handler;
-use tockb_sdk::commands::types::Opts;
 
 fn main() -> Result<()> {
+    env_logger::init();
     let opts: Opts = Opts::parse();
     dbg!(&opts);
     handler(opts)
