5d13d0b8965a5f7fe61a45fd6ba638691da3af70
jacobdenver007 <jacobdenver@163.com>
2020-10-14 22:20:13 +0800

    fix(demo): add miner wallet in bitcoin dockerfile

--- docker/bitcoin/entrypoint.sh
+++ docker/bitcoin/entrypoint.sh
@@ -5,16 +5,25 @@ export PATH=/bitcoin-0.20.0/bin:$PATH
 bitcoind -conf=/etc/bitcoin/bitcoin.conf -rpcport=18443
 sleep 8
 
-bitcoin-cli -conf=/etc/bitcoin/bitcoin.conf -rpcwallet="" importprivkey "cURtxPqTGqaA5oLit5sMszceoEAbiLFsTRz7AHo23piqamtxbzav"
-bitcoin-cli -conf=/etc/bitcoin/bitcoin.conf createwallet user
-bitcoin-cli -conf=/etc/bitcoin/bitcoin.conf -rpcwallet="Alice" importprivkey "cUDfdzioB3SqjbN9vutRTUrpw5EH9srrg6RPibacPo1fGHpfPKqL"
-bitcoin-cli -conf=/etc/bitcoin/bitcoin.conf createwallet signer
-bitcoin-cli -conf=/etc/bitcoin/bitcoin.conf -rpcwallet="Bob" importprivkey "cU9PYTnSkcWoAE15U26JJCwtKiYvTCKYdbWt8e7ovidEGDBwJQ5x"
-bitcoin-cli -conf=/etc/bitcoin/bitcoin.conf generatetoaddress 1 bcrt1q4r9hqljdpfwxu6gp3x7qqedg77r6408dn4wmnf
-bitcoin-cli -conf=/etc/bitcoin/bitcoin.conf generatetoaddress 101 bcrt1q0yszr82fk9q8tu9z9ddxxvwqmlrdycsy378znz
-bitcoin-cli -conf=/etc/bitcoin/bitcoin.conf -rpcwallet="" sendtoaddress bcrt1qfzdcp53u29yt9u5u3d0sx3u2f5xav7sqatfxm2 0.2
-bitcoin-cli -conf=/etc/bitcoin/bitcoin.conf generatetoaddress 101 bcrt1q0yszr82fk9q8tu9z9ddxxvwqmlrdycsy378znz
+MINER_ADDRESS=bcrt1q0yszr82fk9q8tu9z9ddxxvwqmlrdycsy378znz
+ALICE_ADDRESS=bcrt1q4r9hqljdpfwxu6gp3x7qqedg77r6408dn4wmnf
+BOB_ADDRESS=bcrt1qfzdcp53u29yt9u5u3d0sx3u2f5xav7sqatfxm2
+JACK_ADDRESS=bcrt1qgetdwuye26z9hqq05fezjxkkg8mwftrptz98dj
 
-./miner.sh >/dev/null &
+bitcoin-cli -conf=/etc/bitcoin/bitcoin.conf createwallet miner
+bitcoin-cli -conf=/etc/bitcoin/bitcoin.conf -rpcwallet="miner" importprivkey "cURtxPqTGqaA5oLit5sMszceoEAbiLFsTRz7AHo23piqamtxbzav"
+bitcoin-cli -conf=/etc/bitcoin/bitcoin.conf createwallet alice
+bitcoin-cli -conf=/etc/bitcoin/bitcoin.conf -rpcwallet="alice" importprivkey "cUDfdzioB3SqjbN9vutRTUrpw5EH9srrg6RPibacPo1fGHpfPKqL"
+bitcoin-cli -conf=/etc/bitcoin/bitcoin.conf createwallet bob
+bitcoin-cli -conf=/etc/bitcoin/bitcoin.conf -rpcwallet="bob" importprivkey "cU9PYTnSkcWoAE15U26JJCwtKiYvTCKYdbWt8e7ovidEGDBwJQ5x"
+bitcoin-cli -conf=/etc/bitcoin/bitcoin.conf createwallet jack
+bitcoin-cli -conf=/etc/bitcoin/bitcoin.conf -rpcwallet="jack" importprivkey "cT9hz3J53sRNYpytXuHrsexBPCuu4nbKJaKDUDrWsfyTu3nPVP1s"
 
-/bin/bash $@
+bitcoin-cli -conf=/etc/bitcoin/bitcoin.conf generatetoaddress 201 $MINER_ADDRESS
+bitcoin-cli -conf=/etc/bitcoin/bitcoin.conf -rpcwallet="miner" sendtoaddress $ALICE_ADDRESS 2
+bitcoin-cli -conf=/etc/bitcoin/bitcoin.conf -rpcwallet="miner" sendtoaddress $BOB_ADDRESS 0.2
+bitcoin-cli -conf=/etc/bitcoin/bitcoin.conf generatetoaddress 201 $MINER_ADDRESS
+
+./miner.sh >/dev/null $MINER_ADDRESS &
+
+/bin/bash "$@"
--- docker/bitcoin/miner.sh
+++ docker/bitcoin/miner.sh
@@ -3,7 +3,7 @@
 BLOCKTIME=2
 
 while true
-  do
-    bitcoin-cli -conf=/etc/bitcoin/bitcoin.conf generatetoaddress 1 bcrt1q0yszr82fk9q8tu9z9ddxxvwqmlrdycsy378znz
-    sleep $BLOCKTIME
-  done
+do
+  bitcoin-cli -conf=/etc/bitcoin/bitcoin.conf generatetoaddress 1 "$1"
+  sleep $BLOCKTIME
+done
